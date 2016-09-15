786c9dc9d321d7ead8818021a19b37da3015bb57
novaefyre <erouse0963@gmail.com>
2016-09-15 13:51:52 -0400

    Now with years!

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -1,8 +1,9 @@
 package introduction;
 
 public class ProceduralExample {
-  public static void main(String[] args){
-	String[] names = {"Jillian","Joseph","Jordan"};
-    System.out.println("Hi, my name is "+names[0]+".");
+	public static void main(String[] args){
+		String[] names = {"Jillian","Joseph","Jordan"};
+		String[] years = {"Freshman","Sophmore","Junior","Senior"};
+		System.out.println("Hi, my name is "+names[0]+" and I am a "+years[3]+".");
   }
 }
