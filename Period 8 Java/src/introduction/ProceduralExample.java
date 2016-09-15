499a2434046225505f7e9d2f1e7d79eb16bf28c7
Student8 <Student8@10.8.33.156>
2016-09-15 14:00:19 -0400

    I think I got it?

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -5,5 +5,7 @@ public class ProceduralExample {
 	String[] names = {"Jillian","Joseph","Jordan"};
 	String[] years = {"Freshman","Sophmore","Junior","Senior"};
 	System.out.println("Hi, my name is "+names[0]+" and I am a "+years[3]+".");
+	System.out.println("Hi, my name is "+names[1]+" and I am a "+years[2]+".");
+	System.out.println("Hi, my name is "+names[2]+" and I am a "+years[1]+".");
   }
 }
