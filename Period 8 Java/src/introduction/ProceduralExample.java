55bb734f4afed293fc5dfcacc9cc5060e370ef40
Student8 <Student8@10.8.33.156>
2016-09-16 12:44:58 -0400

    NOW WITH FOR LOOPS!!!

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -4,8 +4,11 @@ public class ProceduralExample {
   public static void main(String[] args){
 	String[] names = {"Jillian","Joseph","Jordan"};
 	String[] years = {"Freshman","Sophmore","Junior","Senior"};
-	System.out.println("Hi, my name is "+names[0]+" and I am a "+years[3]+".");
+	for(int i=0; i<3; i++){
+		System.out.println("Hi, my name is "+names[i]+" and I am a "+years[i+1]+".");
+	}
+	/*System.out.println("Hi, my name is "+names[0]+" and I am a "+years[3]+".");
 	System.out.println("Hi, my name is "+names[1]+" and I am a "+years[2]+".");
-	System.out.println("Hi, my name is "+names[2]+" and I am a "+years[1]+".");
+	System.out.println("Hi, my name is "+names[2]+" and I am a "+years[1]+".");*/
   }
 }
