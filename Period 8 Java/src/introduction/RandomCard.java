fda3657a31c808b7005dba6f4adc43f4826b64ec
Student8 <Student8@10.8.33.156>
2016-09-22 14:03:38 -0400

    So. Many. Errors. (also, how i reset array?)

--- Period 8 Java/src/introduction/RandomCard.java
+++ Period 8 Java/src/introduction/RandomCard.java
@@ -3,15 +3,38 @@ package introduction;
 public class RandomCard {
 	static String[] cardNames = {"King","Queen","Jack","Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two","Ace"};
 	static String[] cardSuits = {"Hearts","Spades","Diamonds","Clubs"};
+	static String[] usedCards = {};
 
 	public static void main(String[] args) {
-		double rand = Math.random();
-		int nameIdx = (int) (cardNames.length*rand);
-		double rand2 = Math.random();
-		int suitIdx = (int) (cardSuits.length*rand2);
-		String name = cardNames[nameIdx];
-		String suit = cardSuits[suitIdx];
-		System.out.println(name+" of "+suit);
+		boolean thisCardUsed = false;
+		int i = 0;
+		double rand;
+		int nameIdx;
+		double rand2;
+		int suitIdx;
+		String name;
+		String suit;
+		String cardUsed;
+		while(!thisCardUsed){
+			rand = Math.random();
+			nameIdx = (int) (cardNames.length*rand);
+			rand2 = Math.random();
+			suitIdx = (int) (cardSuits.length*rand2);
+			name = cardNames[nameIdx];
+			suit = cardSuits[suitIdx];
+			cardUsed = name+" of "+suit;
+			if(usedCards.length == 0 || usedCards[i] != cardUsed){
+				usedCards[i] = cardUsed;
+				i++;
+				thisCardUsed = true;
+			}
+		}
+		if(i == 52){
+			System.out.println("All cards used. Resetting.");
+			i = 0;
+			usedCards = {};
+		}
+		System.out.println(cardUsed);
 	}
 
 }
--- Period 8 Java/src/introduction/StringPractice.java
+++ Period 8 Java/src/introduction/StringPractice.java
@@ -1,8 +1,43 @@
 package introduction;
 
+import java.util.Scanner;
+
 public class StringPractice {
 
+	static Scanner input;
+	static String user;
+
 	public static void main(String[] args) {
+		//demonstrateStringMethods();
+		createFields();
+		promptName();
+		promptInput();
+	}
+
+	public static void promptName() {
+		print("Enter your name.");
+		user = input.nextLine();
+		print("Glad to meet you, "+user+". For the rest of time, I will can you "+user+".");
+	}
+
+	public static void promptForever(){
+		while(0<1)
+			promptInput();
+	}
+
+	public static void promptInput() {
+		print("Please type something, "+user+".");
+		String userInput = input.nextLine();
+		print("Congratulations! You typed: "+userInput+", "+user);
+	}
+
+	private static void createFields() {
+		input = new Scanner(System.in);
+		user = "";
+	}
+
+	public static void demonstrateStringMethods(){
+
 		String text1 = new String("Hello World");
 		String text2 = "Hello World";//same as above
 		//System.out.println("Hello World");
@@ -11,18 +46,44 @@ public class StringPractice {
 		}
 		print(text1);
 		print(text2);
-		
+
 		String word1 = "Aardvark";
 		String word2 = "Zyzzyva";
 		if(word1.compareTo(word2)<0){//this is, obviously, how you compare strings and many other objects.
 			print("word1 is greater than word2");
 		}
-		
 
 	}
-	
+
 	public static void print(String s){
-		System.out.println(s);
+		String printString = "";// = s;
+		int cutoff = 10;
+		if(s.length()>cutoff){
+			for(int i=0; i*cutoff<s.length(); i++){
+				printString += getCut(s, cutoff, i+1)+ "\n";
+			}
+		}
+		System.out.println(printString);
 	}
 
-}//If unsure about object's fields or methods, look up API
+	private static String getCut(String s, int cutoff, int cut){
+		int cutIndex = cut*cutoff;
+		if(cutIndex>s.length()){
+			cutIndex = s.length();
+		}
+		String currentCut = s.substring(0, cutIndex);
+		int indexOfLastSpace = currentCut.length()-1;
+		//start at last index, go backwards
+		for(int i = currentCut.length()-1; i>=0; i--){
+			String letter = currentCut.substring(i, i+1);
+			if(letter.equals(" ")){
+				indexOfLastSpace = i;
+				break;
+			}
+			//shorten the cut to end on a space
+			currentCut = currentCut.substring(0, indexOfLastSpace);
+		}
+		return currentCut;
+	}
+	//If unsure about object's fields or methods, look up API
+}
