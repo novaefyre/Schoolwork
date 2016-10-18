78bc93547a0dd2fd604b64096f2b3130d2f52744
Eddie <novaefyre@gmail.com>
2016-10-18 16:15:37 -0400

    stuff

---
+++ Period 8 Java/src/arrays/ArrayMethods.java
@@ -0,0 +1,44 @@
+package arrays;
+
+public class ArrayMethods {
+	
+	public static void main(String[] args) {
+	    
+	     /**
+	      * IMPORTANT NOTE: 
+	      * This homework assignment will be weighted 4x.
+	      * DO NOT ASSUME my tests are perfect! If you have code that you think should work, 
+	      * but you keep failing the tests PLEASE bring it to my attention so that I can fix the tests
+	      * DO NOT spend hours and hours trying to fix perfect code just because my test
+	      * says that it isn't perfect!
+	      * */
+	    }
+	    
+	    public static int searchUnsorted(int[] arrayToSearch, int key){
+	    /**
+	     * this method take an unsorted int array (arrayToSearch) and returns an 
+	     * int corresponding to the index of a key, if it is in the array
+	     * if the key is not in the array, this method returns -1
+	     * */
+	     return -1;
+	    }
+	    
+	    public static int searchSorted(int[] sortedArrayToSearch, int key){
+	    /**
+	     * this method is exactly like the one above, except the parameter sortedArrayToSearch will
+	     * always be sorted in DESCENDING order. Again return the index of the key or return -1
+	     * if the key is not in the array
+	     * 
+	     * Note: You should attempt to write a method that is more efficient that searchUnsorted
+	     * */
+	     return -1;
+	    }
+	    
+	    public static boolean isSorted(int[] array){
+	        /**
+	         * This method takes an in array as a parameter and returns 'true' if the array is already sorted in DESCENDING order
+	         * */
+	    	
+	        return false;
+	    }
+}
