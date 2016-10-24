f9229f4cfb7e2c1b34e31d551fa9436a7cc73189
Student8 <Student8@10.8.33.156>
2016-10-24 14:03:33 -0400

    array methods and MOOOORE

--- Period 8 Java/src/arrays/ArrayMethods.java
+++ Period 8 Java/src/arrays/ArrayMethods.java
@@ -1,27 +1,25 @@
 package arrays;
 
 public class ArrayMethods {
-	
-	public static void main(String[] args) {
 
-		/**
-		 * IMPORTANT NOTE:
-		 * This homework assignment will be weighted 4x.
-		 * DO NOT ASSUME my tests are perfect! If you have code that you think should work,
-		 * but you keep failing the tests PLEASE bring it to my attention so that I can fix the tests
-		 * DO NOT spend hours and hours trying to fix perfect code just because my test
-		 * says that it isn't perfect!
-		 */
-		int[] x = {2,0,3,4,8,7,5};
-		isSorted(x);
-	    }
-	    
+    public static void main(String[] args) {
+    
+     /**
+      * IMPORTANT NOTE: 
+      * This homework assignment will be weighted 4x.
+      * DO NOT ASSUME my tests are perfect! If you have code that you think should work, 
+      * but you keep failing the tests PLEASE bring it to my attention so that I can fix the tests
+      * DO NOT spend hours and hours trying to fix perfect code just because my test
+      * says that it isn't perfect!
+      * */
+    }
+    
     public static int searchUnsorted(int[] arrayToSearch, int key){
-    	/**
-    	 * this method take an unsorted int array (arrayToSearch) and returns an
-    	 * int corresponding to the index of a key, if it is in the array
-    	 * if the key is not in the array, this method returns -1
-    	 */
+    /**
+     * this method take an unsorted int array (arrayToSearch) and returns an 
+     * int corresponding to the index of a key, if it is in the array
+     * if the key is not in the array, this method returns -1
+     * */
     	for(int i = 0; i < arrayToSearch.length; i++){
     		if(arrayToSearch[i] == key){
     			return i;
@@ -30,68 +28,88 @@ public class ArrayMethods {
     	return -1;
     }
     
-    public static boolean isSorted(int[] array){
-    	/**
-    	 * This method takes an in array as a parameter and returns 'true' if the array is already sorted in DESCENDING order
-    	 */
-    	for(int i = 0; i < array.length-1; i++){
-    		if(array[i]>array[i+1])
-    			return false;
-    	}
-        return true;
+    public static int searchSorted(int[] sortedArrayToSearch, int key){
+    /**
+     * this method is exactly like the one above, except the parameter sortedArrayToSearch will
+     * always be sorted in DESCENDING order. Again return the index of the key or return -1
+     * if the key is not in the array
+     * 
+     * Note: You should attempt to write a method that is more efficient that searchUnsorted
+     * */
+        if(key > sortedArrayToSearch[0]){
+            return -1;
+        }else if(key > sortedArrayToSearch[sortedArrayToSearch.length-1]){
+            return -1;
+        }else{
+        	for(int i = 0; i < sortedArrayToSearch.length; i++){
+        		if(sortedArrayToSearch[i] == key){
+        			return i;
+        		}
+        	}
+        	return -1;
+        }
     }
     
+    public static boolean isSorted(int[] array){
+        /**
+         * This method takes an in array as a parameter and returns 'true' if the array is already sorted in DESCENDING order
+         * */
+	    	for(int i = 0; i < array.length-1; i++){
+	    		if(array[i]>array[i+1])
+	    			return false;
+	    	}
+	        return true;
+    }
+    
+    
     public static double[] getStats(double[] array){
-    	/**
-    	 * This method return a double[] contain a WHOLE BUNCH of stats
-    	 * The double array must keep the following stats about the array parameter:
-    	 * index 0 = the mean
-    	 * index 1 = the max
-    	 * index 2 = the min
-    	 * index 3 = the median
-    	 * index 4 = the number of values greater than or equal to the mean
-    	 * index 5 = the number of values below the mean
-    	 */
-    	double[] stats = new double[6];
-    	double mean = array[0];
-    	double max = array[0];
-    	double min = array[0];
-    	double median;
-    	double moreThanMean = 0;
-    	double lessThanMean = 0;
-    	for(int i = 1; i < array.length; i++){
-        	 if(array[i]>max){
-        		 max = array[i];
-        	 }
-        	 if(array[i]<min){
-        		 min = array[i];
-        	 }
-        	 mean += array[i];
-        }
-    	
-    	mean = mean/array.length;
-    	for(int i = 0; i <array.length; i++){
-    		if(array[i] < mean){
-    			lessThanMean++;
-    		}else{
-    			moreThanMean++;
-    		}
-    	}
-    		
-    	
-    	if((array.length/2)-((int)array.length/2) != 0){
-    		median = array[(int) (array.length/2)+1];
-    	}else{
-    		median = (array[(int) (array.length/2)]+array[(int) (array.length/2)+1])/2;
-    	}
-    	
-    	stats[0] = mean;
-    	stats[1] = max;
-    	stats[2] = min;
-    	stats[3] = median;
-    	stats[4] = moreThanMean;
-    	stats[5] = lessThanMean;
-    	return stats;
+        /** 
+         * This method return a double[] contain a WHOLE BUNCH of stats
+         * The double array must keep the following stats about the array parameter:
+         * index 0 = the mean
+         * index 1 = the max
+         * index 2 = the min
+         * index 3 = the median
+         * index 4 = the number of values greater than or equal to the mean
+         * index 5 = the number of values below the mean
+         * */
+            sortDoubleArr(array);
+        
+	    	double[] stats = new double[6];
+	    	double mean = 0;
+	    	double max = array[0];
+	    	double min = array[array.length-1];
+	    	double median = 0;
+	    	double moreThanMean = 0;
+	    	double lessThanMean = 0;
+	    	
+	    	for(int i = 0; i < array.length; i++){
+	    	    mean += array[i];
+	    	}
+	    	mean = mean/array.length;
+	    	
+	    	for(int i = 0; i <array.length; i++){
+	    		if(array[i] < mean){
+	    			lessThanMean++;
+	    		}else{
+	    			moreThanMean++;
+	    		}
+	    	}
+	    		
+	    	
+	    	if((array.length/2)-((int)array.length/2) != 0){
+	    		median = array[(int) (array.length/2)+1];
+	    	}else{
+	    		median = (array[(int) (array.length/2)]+array[(int) (array.length/2)+1])/2;
+	    	}
+	    	
+	    	stats[0] = mean;
+	    	stats[1] = max;
+	    	stats[2] = min;
+	    	stats[3] = median;
+	    	stats[4] = moreThanMean;
+	    	stats[5] = lessThanMean;
+	    	return stats;
     }
     
     public static void reverseOrder(int[] array){
@@ -136,4 +154,174 @@ public class ArrayMethods {
     	}
          return differences;
     }
+    
+
+    public static int longestConsecutiveSequence(int[] array){
+        /**This method counts the longest consequtive sequence in an array.
+         * It does not matter where the sequence begins
+         * If there are no consecutive numbers, the method should return '1'
+         * 
+         * Examples:
+         * longestSequence({1,2,3,4,5,8,9}) returns '5', since the sequence '1,2,3,4,5' is 5 integers long 
+         * longestSequence({0,9,10,11,4,3,8,9}) returns '3', since '9,10,11' is 3 integers long
+         * longestSequence({0,9,8,11,4,3,7,9}) returns '1', since there are no consecutive integers
+         * */
+        int longConsSeq = 1;
+        for(int i = 0; i < array.length-1; i++){
+            int curConsSeq = 1;
+            boolean contCheck = true;
+            int j = i;
+            while(contCheck){
+                if(array[j] == array[j+1]-1){
+                    curConsSeq++;
+                    j++;
+                }else{
+                    contCheck = false;
+                }
+            }
+            i += j;
+            if(curConsSeq > longConsSeq){
+                longConsSeq = curConsSeq;
+            }
+        }
+        return longConsSeq;
+    }
+
+    public static int longestSharedSequence(int[] array1, int[] array2){
+        /**This method counts the longest unbroken, shared sequence in TWO arrays.
+         * The sequence does NOT have to be a consecutive sequence
+         * It does NOT matter where the sequence begins, the arrays might not be the same length
+         * 
+         * Examples:
+         * longestSequence({9,6,3,4,3,8,9}, {9,6,3,4,3,6,7}) returns '5', since the sequence '9,6,3,4,3' is in both arrays and is 5 integers long 
+         * longestSequence({0,9,6,3,4,3,8,9}, {1,2,9,6,3,4,3,6,7}) returns '5', 
+         *          since the sequence '9,6,3,4,3' is in both arrays and is 5 integers long, it doesn't matter that the sequence begins at different indices 
+         * longestSequence({9,6,1,4,3,6,7,9}, {9,6,5,8,3,6,7,0}) returns '3', since the sequence '3,6,7' is in both arrays and is 3 integers long
+         * */
+        int max = 0;
+        int count = 0;
+        for(int seqStart = 0; seqStart < array1.length; seqStart++){
+        	int seqEnd = seqStart;
+        	int[] seq = getSequence(seqStart, seqEnd, array1);
+        	if(checkSequence(seq, array2)){
+        		count++;
+        		if(count < max){
+        			max = count;
+        		}
+        	}
+        	//reset count
+        	count = 0;
+        }
+        
+        return max;
+    }
+
+    //true if seq is inside of array
+    private static boolean checkSequence(int[] seq, int[] array) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	//returns sub array of array from seqStart to seqEnd
+    public static int[] getSequence(int seqStart, int seqEnd, int[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static int[] generateDistinctItemsList(int n){
+        /**
+         * This method needs to generate an int[] of length n that contains distinct, random integers
+         * between 1 and 2n 
+         * The method will be tested by verifying that the array you return is n items long,
+         * contains only entries between 1 and 2n (inclusive) and has no duplicates
+         * 
+         * */
+    /*    int[] array = new int[n];
+        for(int i = 0; i < array.length; i++){
+            array[i] = i+n;
+        }*/
+        return null;
+    }
+    
+    
+    public static void cycleThrough(int[] array, int n){
+        /** This problem represents people moving through a line.
+         * Once they get to the front of the line, they get what they've been waiting for, then they 
+         * immediately go to the end of the line and "cycle through" again.
+         * 
+         * This method reorders the array so that it represents what the array would look like
+         * after it had been cycled through an n number of times.
+         * For example, cycleThrough({2,1,5,6}}, 1) after executing, array == {1,5,6,2} 
+         * because '2' (the person at the front of the line) cycled through and is now at the end of the line
+         * 
+         * cycleThrough({3,7,4,2,8,6,2,9}}, 2) after executing, array == {4,2,8,6,2,9,3,7} 
+         * because '3' and '7' (the TWO people at the front of the line) cycled through and are now at the end of the line
+         * 
+         * Likewise,
+         * cycleThrough({3,7,4,2,8,6,2,9}}, 0) after executing, array == {3,7,4,2,8,6,2,9}  (no movement)
+         * and the most interesting case, 
+         * cycleThrough({3,7,4,2,8,6,2,9}}, 49) after executing, array == {7,4,2,8,6,2,9,3}  
+         * Because after cycling through 49 times, everyone had a chance to go through 6 times, but '3'
+         * was able to go through one more time than anyone else
+         * 
+         * CHALLENGE
+         * For extra credit, make your method handle NEGATIVE n
+         * */
+        for(int i = 0; i < n; i++){
+            int placeholder = array[0];
+            for(int j = 0; j < array.length-1; j++){
+                array[j] = array[j+1];
+            }
+            array[array.length-1] = placeholder;
+        }
+    }
+    
+    //UTILITIES
+    
+    public static void shuffle(Object[] array) {
+		for(int i = 0; i < array.length; i++){
+			int random = (int)(Math.random()*6);
+			swap(array, i, random);
+		}
+	}
+
+	public static void swap(Object[] array, int a, int b) {
+		Object placeholder = array[b];
+		array[b] = array[a];
+		array[a] = placeholder;
+	}
+	
+	public static void swapInt(int[] array, int a, int b){
+	    int placeholder = array[b];
+		array[b] = array[a];
+		array[a] = placeholder;
+	}
+	
+	public static void swapDouble(double[] array, int a, int b){
+	    double placeholder = array[b];
+		array[b] = array[a];
+		array[a] = placeholder;
+	}
+	
+	public static void sortDoubleArr(double[] array){
+	    while(!isSortedDouble(array)){
+	        for(int i = 0; i < array.length; i++){
+	            if(array[i] < array[i+1]){
+	                swapDouble(array, i, i+1);
+	            }
+	        }
+	    }
+	}
+	
+	public static boolean isSortedDouble(double[] array){
+        /**
+         * This method takes a double array as a parameter and returns 'true' if the array is already sorted in DESCENDING order
+         * */
+	    	for(int i = 0; i < array.length-1; i++){
+	    		if(array[i]>array[i+1])
+	    			return false;
+	    	}
+	        return true;
+    }
+
 }
